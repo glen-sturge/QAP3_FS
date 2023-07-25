c8fda45674d37ddab7e5e4d6a897561ef7ce280a
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-07-25 18:06:39 -0230

    Late update. Several updates all around.

---
+++ services/products.dal.js
@@ -0,0 +1,78 @@
+const dal = require("./db");
+
+//get all products
+const getProducts = () => {
+  if (DEBUG) console.log("products.dal.getProducts()");
+  return new Promise((resolve, reject) => {
+    const sql =
+      "SELECT product_id, product_name, product_description, product_price, product_image, category_id FROM product\
+            ORDER BY category_id\
+            ,product_id ASC;";
+    dal.query(sql, [], (err, result) => {
+      if (err) {
+        //logging here
+        if (DEBUG) console.log("Error executing query: " + err);
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
+};
+
+const getProductById = (id) => {
+  if (DEBUG) console.log("products.dal.getProductById()");
+  return new Promise((resolve, reject) => {
+    const sql =
+      "SELECT product_id, product_name, product_description, product_price, product_image, category_id FROM product WHERE product_id = $1";
+    dal.query(sql, [id], (err, result) => {
+      if (err) {
+        //logging here
+        if (DEBUG) console.log("Error executing query: " + err);
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
+};
+
+const addProduct = (
+  product_name,
+  product_description,
+  product_price,
+  product_image,
+  category_id
+) => {
+  if (DEBUG) console.log("products.dal.addProduct()");
+  return new Promise((resolve, reject) => {
+    const sql =
+      "INSERT INTO product (product_name, product_description, product_price, product_image, category_id) \
+        VALUES ($1, $2, $3, $4, $5);";
+    dal.query(
+      sql,
+      [
+        product_name,
+        product_description,
+        product_price,
+        product_image,
+        category_id,
+      ],
+      (err, result) => {
+        if (err) {
+          //logging here
+          if (DEBUG) console.log("Error executing query: " + err);
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  addProduct,
+};
