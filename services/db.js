ae25366090015e1ddf9cd56c71fee413fcc3a901
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-07-29 11:26:32 -0230

    Delete db.js

--- services/db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "qap3",
-  password: "!N00bt00b",
-  port: 5432,
-});
-module.exports = pool;
